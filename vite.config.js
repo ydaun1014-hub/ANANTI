6819611693b7075a749fc9b2a5f5390206a125a9
unknown <ydaun1014@gmail.com>
2026-06-15 17:37:04 +0900

    remove node_modules

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  root: "ananti",
   plugins: [react()],
-  base: "/memo-app/",
-  build: {
-    outDir: "../dist",
-    emptyOutDir: true,
-  },
+  base: "/ANANTI/",
 });
